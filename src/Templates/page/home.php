62cea78a6e707e8feafb95c30e21fb2f2550dbf7
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-06-20 14:29:53 +0200

    enc css home

--- src/Templates/page/home.php
+++ src/Templates/page/home.php
@@ -16,7 +16,7 @@ require_once _ROOTPATH_ . '/src/Entity/auth.php';?>
                         🚗 Réservez votre place grâce à un système de crédits simple et sécurisé<br>
                         🤝 Rencontrez des personnes qui partagent vos valeurs<br>
             </p>
-            <button class="btn btn-primary">
+            <button class="btn btn-home">
                 <?php  if (isset($_SESSION['user'])): ?>
                         <a href="?controller=page&action=about">A propos</a>
                     <?php else: ?>
@@ -28,10 +28,10 @@ require_once _ROOTPATH_ . '/src/Entity/auth.php';?>
 </div>
 
 <!--Galerie ville accessible par Ecoride-->
-<section class="text-gray-600 body-font">
+<section class="body-font">
     <div class="container px-5 py-24 mx-auto">
         <div class="flex flex-col text-center w-full mb-20">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Visitez ces villes grace à Eco'ride</h1>
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4">Visitez ces villes grace à Eco'ride</h1>
             <p class="lg:w-2/3 mx-auto leading-relaxed text-lg">De nouveaux trajets sont proposés régulièrement grace à notre grande communauté d'Eco'Rider! </p>
         </div>
         <div class="flex flex-wrap -m-4">
@@ -94,25 +94,27 @@ require_once _ROOTPATH_ . '/src/Entity/auth.php';?>
 </section>
 
 <!--SearchBar-->
-<section class="text-gray-600 body-font">
+<section class="body-font">
     <div class="container px-5 py-12 mx-auto">
         <div class="flex flex-col text-center w-full mb-8">
-            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Trouvez votre trajet dès maintenant !</h1>
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2">Trouvez votre trajet dès maintenant !</h1>
         </div>
         <div class="flex lg:w-1/1 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
             <div class="relative flex-grow w-full">
-                <label for="full-name" class="leading-7 text-lg text-gray-600">Ville de départ</label>
-                <input type="text" id="full-name" name="full-name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-transparent focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                <label for="full-name" class="leading-7 text-lg">Ville de départ</label>
+                <input type="text" id="full-name" name="full-name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
             </div>
             <div class="relative flex-grow w-full">
-                <label for="email" class="leading-7 text-lg text-gray-600">Ville d'arrivée</label>
-                <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-transparent focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                <label for="email" class="leading-7 text-lg">Ville d'arrivée</label>
+                <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
             </div>
             <div class="relative flex-grow w-full">
-                <label for="date" class="leading-7 text-lg text-gray-600">date</label>
-                <input type="date" id="date" name="date" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-transparent focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                <label for="date" class="leading-7 text-lg">date</label>
+                <input type="date" id="date" name="date" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-transparent focus:ring-2 focus:ring-green-200 text-base outline-none text-black py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
             </div>
-            <button class="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Rechercher</button>
+            <button class="border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg btn-search">
+                <a href="">Rechercher</a>
+            </button>
         </div>
     </div>
 </section>
