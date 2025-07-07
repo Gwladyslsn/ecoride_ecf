2bd2e930113f3c2455df22ab534e1134d62cd697
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-07-07 16:58:21 +0200

    update : in progress

--- src/Templates/page/history.php
+++ src/Templates/page/history.php
@@ -2,7 +2,8 @@
 
 require_once _ROOTPATH_ . '/src/Templates/header.php'; ?>
 
-<h1>Historique des trajets users</h1>
+<h1 class="text-center text-3xl mt-20 mb-10">Historique des trajets utilisateurs</h1>
+<p>Page en cours de construction</p>
 
 <?php
 require_once _ROOTPATH_ . '/src/Templates/footer.php'; ?>
