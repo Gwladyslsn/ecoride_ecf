2c5678afbeccdf52d753fffff8e2c76e9e8d843b
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-07-21 00:13:33 +0200

    change last maj

--- src/Templates/header.php
+++ src/Templates/header.php
@@ -9,7 +9,7 @@ require_once _ROOTPATH_ . '/src/Entity/auth.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= BASE_URL ?>asset/css/main.css">
+    <link rel="stylesheet" href="/asset/css/main.css">
     <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <meta description="Voyagez, partagez grâce à l'écologie">
