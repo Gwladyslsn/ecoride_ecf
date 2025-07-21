acef26d47ca733a7eea70c46444c2931c8e66649
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-07-21 21:43:24 +0200

    maj

--- src/Entity/searchTrip.php
+++ src/Entity/searchTrip.php
@@ -9,17 +9,17 @@ function getAllTrips(PDO $pdo): array
         FROM carpooling
         JOIN car ON carpooling.id_car = car.id_car
         JOIN user ON car.id_user = user.id_user
-        WHERE carpooling.departure_date >= CURRENT_DATE
+        WHERE DATE (carpooling.departure_date) >= CURRENT_DATE
         ORDER BY carpooling.departure_date ASC;";
 
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
     $trips = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    if (!$trips) {
+    /*if (!$trips) {
         echo json_encode(['success' => false, 'message' => 'Aucun trajet']);
         exit;
-    }
+    }*/
 
     return $trips;
 }
