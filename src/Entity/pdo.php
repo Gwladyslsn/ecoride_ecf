b741ac56c057b2ec1e0d2c97c5213ac90671c391
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-07-21 16:04:38 +0200

    Fix pdo

--- src/Entity/pdo.php
+++ src/Entity/pdo.php
@@ -6,7 +6,11 @@ try
 {
     $config = parse_ini_file(_ROOTPATH_ . ".env");
     global $pdo;
-    $pdo = new PDO("mysql:dbname={$config["db_name"]};host={$config["db_host"]};charset=utf8mb4", $config["db_user"], $config["db_password"]);
+    $pdo = new PDO(
+        'mysql:host=' . getenv('db_host') . ';dbname=' . getenv('db_name') . ';charset=utf8mb4',
+        getenv('db_user'),
+        getenv('db_password')
+    );
 
     if (!isset($pdo)) {
     die("La connexion PDO n'a pas été créée !");
