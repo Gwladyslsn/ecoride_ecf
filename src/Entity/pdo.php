73e2ca853d8be5b214cbd314ffe74ea2e8c4f50c
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-07-21 16:07:13 +0200

    Fix pdo

--- src/Entity/pdo.php
+++ src/Entity/pdo.php
@@ -4,7 +4,6 @@
 
 try
 {
-    $config = parse_ini_file(_ROOTPATH_ . ".env");
     global $pdo;
     $pdo = new PDO(
         'mysql:host=' . getenv('db_host') . ';dbname=' . getenv('db_name') . ';charset=utf8mb4',
