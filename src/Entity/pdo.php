53404d8973bb234f690028758889adc5739431fb
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-07-21 19:51:07 +0200

    fix pdo.php

--- src/Entity/pdo.php
+++ src/Entity/pdo.php
@@ -1,26 +1,6 @@
 <?php
 
 //Connexion à la base de données
-/*
-try
-{
-    $config = parse_ini_file(_ROOTPATH_ . ".env");
-    global $pdo;
-    $pdo = new PDO("mysql:dbname={$config["db_name"]};host={$config["db_host"]};charset=utf8mb4", $config["db_user"], $config["db_password"]);
-
-    if (!isset($pdo)) {
-    die("La connexion PDO n'a pas été créée !");
-}
-}
-catch (Exception $e)
-{
-die('Erreur : ' . $e->getMessage());
-}*/
-
-//Connexion à la base de données
-<?php
-
-//Connexion à la base de données
 
 try {
     global $pdo;
@@ -99,5 +79,3 @@ try {
         die('Erreur : ' . $e->getMessage());
     }
 }
-
-?>
