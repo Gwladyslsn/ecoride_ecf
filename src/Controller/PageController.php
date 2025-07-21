2d81a1d223762a99fc25c9d97372506f5365c894
Gwladyslsn <gwladyslaisne@outlook.fr>
2025-07-21 09:20:06 +0200

    Fix chemin vue home

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -82,7 +82,7 @@ class PageController extends Controller
     protected function home()
     {
         echo 'On est dans la fonction home()';
-        $this->render('/Templates/page/home', []);
+        $this->render('Templates/page/home', []);
     }
 
     protected function register()
